bf4d8b2672757c8328d141e63e0c9496697284c2
UEADAVID24 <tedmaalvarado1998@gmail.com>
2026-02-09 23:49:13 -0500

    Configurar Flask para Render (host y port)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -12,5 +13,6 @@ def inicio():
 def usuario():
     return "Bienvenido, Clinton. Tu turno está registrado correctamente."
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
